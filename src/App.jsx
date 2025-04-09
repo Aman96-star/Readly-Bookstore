2caebcb51b789c2ccdf53efa022b93a0fb1f3d73
Aman <99051262+Aman96-star@users.noreply.github.com>
2025-04-09 22:34:34 +0530

    fiction and nonfiction completed additional main section ids created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 
 import './App.css'
 import Navbar from './components/navbar'
@@ -7,55 +6,16 @@ import Fiction from './components/fiction'
 import NonFiction from './components/nonfiction'
 
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
     <>
      <div className='h-fit w-full overflow-x-hidden'>
       <Navbar/>
       <Headpage/>
-      {/* category list books type  */}
-    <Fiction/>
-{/* Adventure stories
-Classics
-Crime
-Fairy tales, fables, and folk tales
-Fantasy
-Historical fiction
-Horror
-Humour and satire
-Literary fiction
-Mystery
-Poetry
-Plays
-Romance
-Science fiction
-Short stories
-Thrillers
-War
-Women’s fiction
-Young adult      */}
+      <Fiction/>
       <NonFiction/>
-{/* Autobiography and memoir
-Biography
-Essays
-Non-fiction novel
-Self-help */}
-
-
-
-      {/* main ecommerce items items gird map is used */}
-      {/*1st is top sellers 2nd is recommended for you  */}
-
-      {/* top authers */}
-      {/*feedback or rating*/}
-      {/* Footer */}
-      {/* new  */}
-      {/* sdjosudiou */}
-     {/* Strak */}
-     {/* streak 2  */}
-     {/* aadmakmd */}
+      
      </div>
     </>
   )
